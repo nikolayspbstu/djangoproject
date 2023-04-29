1a0bba1ab0a89211de7fa9d8142375c4da767db6
kolyaspbstu <prostonick228@mail.ru>
2023-04-29 14:50:44 +0300

    add

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from parser import collect,lower_func
 # Create your views here.
 
 
@@ -12,3 +13,8 @@ def about(request):
 
     return render(request,'main/about.html')
 
+def teacherteam(request):
+    natural = list(range(1,2))
+    dict_list = collect()
+    text='д.ф.-м.н., профессор, научный руководитель магистерской программы "Модели и высокопроизводительные вычисления в физической гидрогазодинамике"'
+    return render(request, 'main/personal.html', {'data': dict_list,'natural':natural})
